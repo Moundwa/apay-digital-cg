020017db8862134ee3a333bf922139d13f33c766
Moundwa <arsene4561@gmail.com>
2026-06-05 09:37:36 +0100

    Extend Tailwind theme with custom font family
    Added custom font family 'inter' to the Tailwind theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   content: [
     './app/*/.{js,ts,jsx,tsx,mdx}',
     './components/*/.{js,ts,jsx,tsx,mdx}',
+    './pages/*/.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
+    },
   },
   plugins: [],
 }
-
