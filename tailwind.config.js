cb8594b1c2476b5e50e02426fa8fdd827d067657
Moundwa <arsene4561@gmail.com>
2026-06-05 09:50:16 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/*/.{js,ts,jsx,tsx,mdx}',
-    './components/*/.{js,ts,jsx,tsx,mdx}',
-    './pages/*/.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)'],
+        gaming: ['Orbitron', 'sans-serif'],
       },
+      colors: {
+        cyan: {
+          400: '#22d3ee',
+          500: '#06b6d4',
+        }
+      }
     },
   },
   plugins: [],
